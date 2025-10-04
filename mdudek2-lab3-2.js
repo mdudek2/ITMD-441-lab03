1657920779b7ee394c22a7d3f0d050023f8a498b
mdudek2 <mdudek2@hawk.iit.edu>
2025-10-04 17:43:45 -0500

    implemented function 2

---
+++ mdudek2-lab3-2.js
@@ -0,0 +1,22 @@
+// function definition
+function countVowels(word) {
+  
+  var count = 0
+  for(i = 0; i < word.length; i++) {
+    if (/[aeiou]/i.test(word[i])) {
+        count +=1;
+    }
+  }
+  
+  return count
+}
+
+// strings to test
+var s1 = "teststring"
+var s2 = "Apple Pie"
+var s3 = "Illinois Tech"
+
+// output
+console.log(s1 + ": vowels: " + countVowels(s1))
+console.log(s2 + ": vowels: " + countVowels(s2))
+console.log(s3 + ": vowels: " + countVowels(s3))
