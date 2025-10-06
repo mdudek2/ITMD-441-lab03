1ddc4043d6b11bc524f32867043b260390ce97d9
mdudek2 <mdudek2@hawk.iit.edu>
2025-10-06 08:50:45 -0500

    implemented excercise3

---
+++ mdudek2-lab3-3.js
@@ -0,0 +1,17 @@
+// function defininition
+
+
+// test arrays
+var arr1 = [6,2,5,0,1,56]
+var arr2 = [9,5,2,1,12,45,90]
+var arr3 = [17,13,34,89,2,7]
+
+function sortNumbers(arr) {
+  arr.sort()
+  sorted_arr = arr
+  return sorted_arr
+}
+
+console.log("Original Array: " + arr1 + "Sorted Array: " + sortNumbers(arr1))
+console.log("Original Array: " + arr2 + "Sorted Array: " + sortNumbers(arr2))
+console.log("Original Array: " + arr3 + "Sorted Array: " + sortNumbers(arr3))
