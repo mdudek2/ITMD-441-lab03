bc83dc97f14c19b4e14c91b50cda86f611a7fd62
mdudek2 <mdudek2@hawk.iit.edu>
2025-10-06 09:06:11 -0500

    implemented excercise4

---
+++ mdudek2-lab3-4.js
@@ -0,0 +1,14 @@
+// test values
+var test1 = 0
+var test2 = 72
+var test3 = 100
+
+// function definition
+function celsiusToFahrenheit(temp_c) {
+  return ((temp_c * (9/5)) + 32)
+}
+
+// output
+console.log(test1 + " celsius = " + celsiusToFahrenheit(test1) + " fahrenheit.")
+console.log(test2 + " celsius = " + celsiusToFahrenheit(test2) + " fahrenheit.")
+console.log(test3 + " celsius = " + celsiusToFahrenheit(test3) + " fahrenheit.")
